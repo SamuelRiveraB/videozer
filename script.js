3e931942ca75b4f0f0bd43c89b19941c1a5df627
Samuel <samuelrivba@gmail.com>
2024-01-16 15:10:25 -0500

    Add Play Pause functions

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+const video = document.querySelector('video')
+const progressRange = document.querySelector('.progress-range')
+const progressBar = document.querySelector('.progress-bar')
+const playBtn = document.getElementById('play-btn')
+const volumeIcon = document.getElementById('volume-icon')
+const volumeRange = document.querySelector('volume-range')
+const volumeBar = document.querySelector('.volume-bar')
+const currentTime = document.querySelector('.time-elapsed')
+const duration = document.querySelector('.time-duration')
+const fullscreenBtn = document.querySelector('fullscreen')
+
 
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play')
+    playBtn.title = 'Play'
+}
 
+function togglePlay() {
+    if (video.paused) {
+        video.play()
+        playBtn.classList.replace('fa-play', 'fa-pause')
+        playBtn.title = 'Pause'
+    } else {
+        video.pause()
+        playBtn.classList.replace('fa-pause', 'fa-play')
+        playBtn.title = 'Play'
+    }
+}
+
+video.addEventListener('ended', showPlayIcon)
 
 // Progress Bar ---------------------------------- //
 
@@ -19,3 +47,6 @@
 // Fullscreen ------------------------------- //
 
 
+
+playBtn.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay)
